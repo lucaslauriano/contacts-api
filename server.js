171c3a136af919d7dd90b2da2a9c98829c15e3f2
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2023-10-31 01:59:27 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDb = require('./src/config/dbConnection');
+const errorHandler = require('./src/middleware/errorHandler');
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use('/api/contacts', require('./src/routes/contactRoutes'));
+app.use('/api/users', require('./src/routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`server running in port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
